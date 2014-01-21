7557159813d21c4504b75e0548181a21f2900143
crccheck <c@crccheck.com>
2014-01-20 21:49:14 -0600

    add a link to nowhere

--- echo/urls.py
+++ echo/urls.py
@@ -1,7 +1,9 @@
-import json
-
 from collections import OrderedDict
 from copy import copy
+from operator import concat
+import json
+import random
+import string
 
 from django.conf.urls import patterns, url
 from django.http import HttpResponse
@@ -24,13 +26,29 @@ META = (
 
 class Wow(View):
     """such view."""
+    def get_fake_word(self):
+        n = random.randint(3, 12)
+        return reduce(concat, [random.choice(string.ascii_letters) for __ in range(n)])
+
+    def get_fake_link(self):
+        n = random.randint(1, 5)
+        out = '/'.join([self.get_fake_word() for __ in range(n)])
+        if random.randint(0, 3):
+            # sometimes give a trailing slash
+            out += '/'
+        return out
+
+
     def get(self, request, *args, **kwargs):
         # filter META down to just the standard stuff
         request_data = copy(request)
         meta = [(x, y) for x, y in request_data.META.items() if
             x in META or x.startswith('HTTP_')]
         request_data.META = OrderedDict(sorted(meta))
-        context = {'request': request_data}
+        context = {
+            'next': self.get_fake_link(),
+            'request': request_data,
+        }
 
         # TODO check 'Accept' header
         return_json = request.is_ajax()
@@ -55,6 +73,8 @@ class Wow(View):
         # http://tools.ietf.org/html/draft-ietf-appsawg-xdash-05
         # http://en.wikipedia.org/wiki/List_of_HTTP_header_fields#Responses
         response['Readme'] = 'https://github.com/crccheck/django-request-echo'
+        # https://developers.google.com/webmasters/control-crawl-index/docs/robots_meta_tag
+        response['X-Robots-Tag'] = 'noindex'
         return response
 
     post = get  # TEEHEE
